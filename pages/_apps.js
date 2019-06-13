fba2286e5fad8fee2fa77a4e90f5cf3635dbbb75
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-06-13 13:28:33 +0700

    add less import

---
+++ pages/_apps.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+import '../src/styles/index.less'
+
+export default class CustomApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
--- src/Layout/EditorLayout.js
+++ src/Layout/EditorLayout.js
@@ -1,15 +1,38 @@
-import { Layout } from 'antd'
+import { Layout, Button } from 'antd'
 
 const { Header, Content } = Layout
 
-const EditorLayout = ({ children }) => (
+const EditorLayout = ({ 
+  children,
+  onCompile,
+}) => (
   <Layout
     style={{
-      minHeight: 900,
+      minHeight: '100vh'
     }}
   >
-    <Header>
-      test
+    <Header
+      style={{
+        height: 70
+      }}
+    >
+        <img 
+          src="/static/img/logo.png"
+          style={{
+            padding: 15,
+            height: '60px'
+          }}
+        />
+        <Button 
+          type="primary"
+          icon="play-circle"
+          onClick={onCompile}
+          style={{
+
+          }}
+        >
+          Run
+        </Button>
     </Header>
     <Content
       style={{
