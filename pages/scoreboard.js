0a646a22206294f9c26027c13263ed82c6882b1a
Phatchareeporn <phatchareeporn@thinknet.co.th>
2019-06-13 16:40:30 +0700

    finish scoreboard

--- pages/scoreboard.js
+++ pages/scoreboard.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import '../assets/scoreboard.less'
-import { Button, Layout, List, Avatar } from 'antd'
+import { Button, Layout, List, Avatar, Badge } from 'antd'
+import axios from 'axios'
 import defaultData from '../data/db.json'
 
 const { Header, Content, Footer } = Layout
 
 export default class Scoreboard extends Component {
+  state = {
+    data: [],
+  }
+
+  componentWillMount() {
+    this.getScore()
+  }
+
+  getScore = async () => {
+    const { data } = await axios.get('http://localhost:8080/users')
+    this.setState({
+      data,
+    })
+  }
+  
   sortScore = (data, prop, asc) => {
     return data.sort((a, b) => {
       if (asc) {
@@ -16,21 +32,38 @@ export default class Scoreboard extends Component {
     })
   }
 
-  renderListItem = (item) => {
+  getTopStyle = (rank) => {
+    switch (rank) {
+      case 1:
+        return 'top one'
+        break;
+      case 2:
+        return 'top two'
+        break;
+      case 3:
+        return 'top three'
+        break;
+      default:
+        return ''
+        break;
+    }
+  }
+
+  renderListItem = (item, index) => {
+    console.log(index)
     return (
-      <List.Item>
+      <List.Item className={this.getTopStyle(index+1)}>
         <List.Item.Meta
-          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+          avatar={<Avatar src="https://cdn4.iconfinder.com/data/icons/web-development-6-1/32/tech_user_boss_person-512.png" />}
           title={<a href="https://ant.design">{item.name}</a>}
         />
-          <div>{item.score}</div>
+          <div className={(index < 3) ? 'top-score' : ''}>{item.score}</div>
       </List.Item>
     )
   }
 
   render() {
-    const dataSorted = this.sortScore(defaultData.users, 'score', false)
-
+    const dataSorted = this.sortScore(this.state.data, 'score', false)
     return (
       <div className="scoreboard">
         <Layout className="main-layout">
