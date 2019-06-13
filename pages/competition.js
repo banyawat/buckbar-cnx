dfd0502a71a78278aa8bfb9327207553e3bc2c43
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-06-13 12:52:33 +0700

    add react ace editor

--- pages/competition.js
+++ pages/competition.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
-import ReplitClient from 'repl.it-api'
+import dynamic from 'next/dynamic'
+
+const code = `function add(a, b) {
+  return a + b;
+}
+`
+
+const DynamicComponent = dynamic(() => import('react-ace'),
+{
+  ssr: false
+})
 
 export default class competition extends Component {
+  state = {
+    code,
+  }
+
+  componentDidMount() {
+    require('brace')
+    require('brace/mode/javascript')
+    require('brace/theme/github')
+  }
+
   render() {
     return (
       <div>
-        
+        Editor
+        <DynamicComponent
+          mode="javascript"
+          theme="github"
+          onChange={() => {
+            console.log('test')
+          }}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{$blockScrolling: true}}
+        />
       </div>
     )
   }
