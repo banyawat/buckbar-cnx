0ea90b644bd097aad1d5790b474ce4c9d704ba49
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-06-13 11:35:12 +0700

    asdasdasddsa

---
+++ pages/competition.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReplitClient from 'repl.it-api'
+
+export default class competition extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
