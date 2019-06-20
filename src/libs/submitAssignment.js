917611282ebbf11d90e58ceb34ce86bfd0833c5c
chaowakrit <chaowakrit@thinknet.co.th>
2019-06-20 18:10:32 +0700

    change parameter username to name

--- src/libs/submitAssignment.js
+++ src/libs/submitAssignment.js
@@ -4,10 +4,10 @@ import CONSTANT from '../constants'
 const { SERVICE_URL } = CONSTANT
 const ASSIGNMENT_URL = `${SERVICE_URL}/assignments`
 
-const submitAssignment = async (username, id) => {
-  if (username && id) {
+const submitAssignment = async (name, id) => {
+  if (name && id) {
     await axios.post(ASSIGNMENT_URL, {
-      username,
+      name,
       id,
     })
   }
