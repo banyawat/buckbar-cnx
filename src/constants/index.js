a347e3750e2da5b803f7da0d5ea2411b33276882
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-07-03 11:02:52 +0700

    change ip to localhost

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ import { QUEST_PATTERN } from './questPattern'
 import EDITOR_DEFAULT_PROPS from './editorDefaultProps'
 
 export default {
-  SERVICE_URL: 'http://172.16.128.18:8080',
+  SERVICE_URL: 'http://localhost:8080',
   QUEST_PATTERN,
   EDITOR_DEFAULT_PROPS
 }
